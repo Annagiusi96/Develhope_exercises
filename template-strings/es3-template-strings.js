9a115605182520d815cc96676683f1eb87bf55d9
Annagiusi <148056449+Annagiusi@users.noreply.github.com>
2023-11-23 09:55:19 +0100

    aggiunto es 3 strings

---
+++ template-strings/es3-template-strings.js
@@ -0,0 +1,6 @@
+// Crea una template string che genera un messaggio di saluto con un nome e un messaggio personalizzato.
+
+const name = "John";
+const message = "Welcome to our website!";
+const greeting = `Good Morning, ${name}! ${message}`
+console.log(greeting);

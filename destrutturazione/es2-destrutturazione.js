4999812721c99d255aa878ae80353b9781065d62
Annagiusi <148056449+Annagiusi@users.noreply.github.com>
2023-11-23 10:02:12 +0100

    aggiunto es 2 destrutt

---
+++ destrutturazione/es2-destrutturazione.js
@@ -0,0 +1,16 @@
+// Dato uno studente oggetto nidificato con informazioni sulle proprietà e voti, 
+// utilizza la destrutturazione dell'oggetto per creare variabili nome, 
+// età e voto che contengano i valori dell'oggetto nidificato.
+
+
+const student = {
+    info: { name: "Bob", age: 25 },
+    grades: { math: 95, science: 89 }
+  };
+  
+  const {name, age} = student.info;
+  const {math, science} = student.grades
+
+  console.log(`Sono ${name} e ho ${age} anni. Il mio voto in matematica è: ${math},
+  mentre quello in science è: ${science}`);
+

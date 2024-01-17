64094ef3d1f94e1b4508e2d207c802004994a4ad
Annagiusi <148056449+Annagiusi@users.noreply.github.com>
2024-01-17 10:55:16 +0100

    domande components2

--- components-1/components-1/src/App.jsx
+++ components-1/components-1/src/App.jsx
@@ -1,9 +1,17 @@
 import { Hello } from "./Hello";
+import { Message } from "./Message";
 
 export function App(){
     return(
         <div>
             <Hello></Hello>
+            <Hello></Hello>
+            <Hello></Hello>
+            <Hello></Hello>
+            {/* se utilizzo il componente Hello più volte nell'App componente, viene semplicemente eseguito il renderig
+            del componente Hello tante volte quanto è stato usato */}
+            <Message></Message>
+            {/* se renderizzo il componente Message, viene visualizzato soltanto il suo contenuto, ovvero il <p></p> */}
         </div>
     )
 }

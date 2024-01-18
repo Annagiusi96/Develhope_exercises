f5fe9039576af7bd875e3a37393bae823b1a7ffc
Annagiusi <148056449+Annagiusi@users.noreply.github.com>
2024-01-18 11:31:17 +0100

    conditional-rendering-2

--- conditional-rendering/src/Age.jsx
+++ conditional-rendering/src/Age.jsx
@@ -1,3 +1,10 @@
-export function Age({age}){
-    return <p>Your age is {age}</p>
+// export function Age({age}){
+//     return <p>Your age is {age}</p>
+// }
+
+//CONDITIONAL RENDERING-2
+export function Age({ age }) {
+    return <div>
+        {age > 18 ? <p>Your age is {age}</p> : <p>You are very young</p>}
+    </div>
 }

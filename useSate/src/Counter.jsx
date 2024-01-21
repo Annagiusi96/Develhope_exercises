e3b634c63292918f74c9e14ba75415f578453f22
Annagiusi <148056449+Annagiusi@users.noreply.github.com>
2024-01-21 19:55:19 +0100

    useEffect-es-1

--- useSate/src/Counter.jsx
+++ useSate/src/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export function Counter ({initialValue = 0, incrementAmount}){
 const [counter, setCounter] = useState(initialValue)
@@ -6,6 +6,15 @@ const [counter, setCounter] = useState(initialValue)
     function handleCounterIncrement(){
         setCounter((c)=> c + incrementAmount)
     }
+
+    
+    //ES-1-USEEFFECT:
+    useEffect(()=>{
+        console.log(counter);
+    },[counter])
+
+
+
 // quando si chiama la funzione setCounter, se il valore della variabile di stato dipende dal suo valore precedente,
 // allora è sempre opportuno usare un arrow function in modo che siamo sicuri che il settaggio del valore della varibile di stato 
 // avvenga immediatamente. Se invece passiamo solo il valore immediato che deve avere la variabile di stato, allora

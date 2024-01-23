a0a01a6b2e36e3153b911fa8f7f7acc81ab1e98d
Annagiusi <148056449+Annagiusi@users.noreply.github.com>
2024-01-23 18:09:36 +0100

    es 2 controlled form

--- controlled-forms/src/App.jsx
+++ controlled-forms/src/App.jsx
@@ -1,5 +1,6 @@
 
 import { InteractiveWelcome } from "./InteractiveWelcome";
+import { Login } from "./Login";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <InteractiveWelcome/>
       
+      <Login/>
     </>
   )
 }

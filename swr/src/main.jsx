3031369c6bbe1f72257c579b6803281695de168e
Annagiusi <148056449+Annagiusi@users.noreply.github.com>
2024-02-11 19:44:00 +0100

    es4

--- swr/src/main.jsx
+++ swr/src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { SWRConfig } from 'swr'
+
+const fetcher = url => fetch(url).then((res)=> res.json())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <SWRConfig value={{ fetcher }}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  </SWRConfig>
 )
